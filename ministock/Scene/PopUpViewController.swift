2ebc5666851eacef293441552c89a64ce239e50f
geeyong Yoon <dev.geeyong@gmail.com>
2022-07-12 12:52:08 +0900

    - refactoring popupview

--- ministock/Scene/PopUpViewController.swift
+++ ministock/Scene/PopUpViewController.swift
@@ -1,29 +1,26 @@
 import UIKit
 
-final class PopUpViewController: UIViewController{
+final class PopUpViewController: UIViewController {
     
-    private let containerView: UIView = {
+    private lazy var containerView: UIView = {
         let view = UIView()
         view.backgroundColor = .white
         view.layer.cornerRadius = 16
         view.clipsToBounds = true
         return view
     }()
-    
-    private let dimmedView: UIView = {
+    private lazy var dimmedView: UIView = {
         let view = UIView()
         view.backgroundColor = .black
         view.alpha = 0.6
         return view
     }()
-    
-    private let titleLabel: UILabel = {
+    private lazy var titleLabel: UILabel = {
         let lb = UILabel()
         lb.text = "최근 기간별"
         lb.translatesAutoresizingMaskIntoConstraints = false
         return lb
     }()
-    
     private let collectionBackView: UIView = {
         let uv = UIView()
         uv.layer.cornerRadius = 5
@@ -31,9 +28,7 @@ final class PopUpViewController: UIViewController{
         uv.layer.borderWidth = 1
         return uv
     }()
-    
-    private let collectionView: UICollectionView = {
-        
+    private lazy var collectionView: UICollectionView = {
         let flowLayout = UICollectionViewFlowLayout()
         flowLayout.minimumLineSpacing = 0
         flowLayout.scrollDirection = .horizontal
@@ -41,16 +36,15 @@ final class PopUpViewController: UIViewController{
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         collectionView.backgroundColor = UIColor.white
         collectionView.layer.cornerRadius = 5
-        
+        collectionView.backgroundColor = .systemGray4
         return collectionView
     }()
-    var containerViewHeightConstraint: NSLayoutConstraint?
-    var containerViewBottomConstraint: NSLayoutConstraint?
-    
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        UIView.animate(withDuration: 0.3) {
-            self.containerViewBottomConstraint?.constant = 0 //화면 아래 300만큼 내려가있던게.. 0올라오면서..
+        UIView.animate(withDuration: 0.5) {
+            self.containerView.snp.updateConstraints {
+                $0.bottom.equalToSuperview().offset(0)
+            }
             self.view.layoutIfNeeded()
         }
     }
@@ -63,13 +57,13 @@ final class PopUpViewController: UIViewController{
         collectionView.dataSource = self
         collectionView.delegate = self
         collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
-        setupConstraints()
-        
+        initLayout()
     }
-    
     @objc func animateDismissView() {
         UIView.animate(withDuration: 0.3) {
-            self.containerViewBottomConstraint?.constant = 300 // 다시 화면아래 300
+            self.containerView.snp.updateConstraints {
+                $0.bottom.equalToSuperview().offset(300)
+            }
             self.view.layoutIfNeeded()
         }
         dimmedView.alpha = 0.6
@@ -79,83 +73,50 @@ final class PopUpViewController: UIViewController{
             self.dismiss(animated: false)
         }
     }
-    
-    func setupConstraints() {
-        
+    func initLayout() {
         view.addSubview(dimmedView)
+        dimmedView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
         view.addSubview(containerView)
-        dimmedView.translatesAutoresizingMaskIntoConstraints = false
-        containerView.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            dimmedView.topAnchor.constraint(equalTo: view.topAnchor),
-            dimmedView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            dimmedView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            dimmedView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            
-            containerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            containerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-        ])
-        
-        
-        containerView.heightAnchor.constraint(equalToConstant: 300).isActive = true
-        containerViewBottomConstraint = containerView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 300)
-        
-        containerViewBottomConstraint?.isActive = true
-        
+        containerView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview()
+            $0.height.equalTo(300)
+            $0.bottom.equalToSuperview().offset(300)
+        }
         containerView.addSubview(collectionBackView)
-        collectionBackView.translatesAutoresizingMaskIntoConstraints = false
-        collectionBackView.centerXAnchor.constraint(equalTo: containerView.centerXAnchor).isActive = true
-        collectionBackView.centerYAnchor.constraint(equalTo: containerView.centerYAnchor).isActive = true
-        collectionBackView.widthAnchor.constraint(equalTo: containerView.widthAnchor, multiplier: 3/4).isActive = true
-        collectionBackView.heightAnchor.constraint(equalTo: containerView.heightAnchor, multiplier: 0.4).isActive = true
-        
+        collectionBackView.snp.makeConstraints {
+            $0.centerX.centerY.equalTo(containerView)
+            $0.width.equalTo(containerView.snp.width).multipliedBy(0.75)
+            $0.height.equalTo(containerView).multipliedBy(0.4)
+        }
         collectionBackView.addSubview(collectionView)
-        
-        collectionView.translatesAutoresizingMaskIntoConstraints = false
-        collectionView.backgroundColor = .systemGray4
-        collectionView.topAnchor.constraint(equalTo: collectionBackView.topAnchor,constant: 0).isActive = true
-        collectionView.leadingAnchor.constraint(equalTo: collectionBackView.leadingAnchor,constant: 0).isActive = true
-        collectionView.trailingAnchor.constraint(equalTo: collectionBackView.trailingAnchor,constant: 0).isActive = true
-        collectionView.bottomAnchor.constraint(equalTo: collectionBackView.bottomAnchor, constant: 0).isActive = true
-        
-        
+        collectionView.snp.makeConstraints {
+            $0.top.leading.trailing.bottom.equalTo(collectionBackView)
+        }
         containerView.addSubview(titleLabel)
-        titleLabel.leadingAnchor.constraint(equalTo: collectionView.leadingAnchor).isActive = true
-        titleLabel.bottomAnchor.constraint(equalTo: collectionView.topAnchor,constant: -15).isActive = true
+        titleLabel.snp.makeConstraints {
+            $0.leading.equalTo(collectionBackView.snp.leading)
+            $0.bottom.equalTo(collectionView.snp.top).offset(-15)
+        }
     }
-    
 }
 extension PopUpViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 6
     }
-    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
         cell.backgroundColor = .white
         return cell
     }
-    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
-        
-    }
-    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
         return CGSize.init(width: collectionBackView.bounds.width/3.0, height: collectionBackView.frame.height/2.0 - 0.5)
-        
     }
-    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 1.0
     }
-    
-    // CollectionView Cell의 옆 간격
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return 1.0
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        
     }
 }
